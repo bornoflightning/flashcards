b349b70c6a648cea6cbb362190c09774087736ce
bornoflightning <victor.escarcega@hotmail.com>
2024-01-15 16:32:50 -0700

    added includes for footer and header and hello file. Also added cookie parser and post request route

--- flashcards.js
+++ flashcards.js
@@ -1,18 +1,35 @@
 const express = require ('express');
+const pug = require('pug');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 
 const app = express();
 
 const port = 3001;
 
+//section for middlewear
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(cookieParser());
 app.set('view engine', 'pug');
 
 
 app.get('/', (req, res) => {
-    res.send("Hello World!!");
+    res.render('index');
+})
+
+app.get('/cards', (req, res) => {
+    res.locals.prompt = "what color is the sky?"
+    res.render('cards');
 })
 
 app.get('/hello', (req, res) => {
-    res.send("Hello, Javascript!")
+    res.render('hello');
+});
+
+app.post('/hello', (req, res) => {
+    res.cookie('username', req.body.username);
+    res.render('hello', {name: req.body.username});
 })
 
 app.listen(port, () => {
